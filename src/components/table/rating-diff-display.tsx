b148f7a31ce5637df821c6d839f7ad4ec2186440
brunecj <jernej.brunec@zav-sava.si>
2025-12-10 12:35:48 +0100

    Set rating diff precision to 2

--- src/components/table/rating-diff-display.tsx
+++ src/components/table/rating-diff-display.tsx
@@ -1,14 +1,15 @@
-function RatingDiffDisplay({ ratingDiff }: { ratingDiff: number }) {
+function RatingDiffDisplay({ ratingDiff }: { ratingDiff?: number }) {
+  if (!ratingDiff) return <></>;
   return ratingDiff > 0 ? (
     <>
       {"("}
-      <span className="text-green-500">+{ratingDiff}</span>
+      <span className="text-green-500">+{ratingDiff.toPrecision(2)}</span>
       {")"}
     </>
   ) : (
     <>
       {"("}
-      <span className="text-red-500">{ratingDiff}</span>
+      <span className="text-red-500">{ratingDiff.toPrecision(2)}</span>
       {")"}
     </>
   );
